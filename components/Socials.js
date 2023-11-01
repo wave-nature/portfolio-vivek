df4f93427e76aa16aa986e5d4b59342e1205f838
wave-nature <vs1010582@gmail.com>
2023-11-01 12:48:08 +0530

    social links added

--- components/Socials.js
+++ components/Socials.js
@@ -10,25 +10,29 @@ const Socials = () => {
   return (
     <div className=" flex items-center gap-x-5 text-lg">
       <Link
-        href=""
+        href="https://www.youtube.com/channel/UCanvCjaT7JmatI0xlo9JXtQ"
+        target="_blank"
         className=" hover:text-accent transition-all outline-lime-300"
       >
         <RiYoutubeLine />
       </Link>
       <Link
-        href=""
+        href="https://www.instagram.com/viveksinghbhadouria/"
+        target="_blank"
         className=" hover:text-accent transition-all outline-lime-300"
       >
         <RiInstagramLine />
       </Link>
       <Link
-        href=""
+        href="https://www.linkedin.com/in/vivek-singh-bhadouria-427649200/"
+        target="_blank"
         className=" hover:text-accent transition-all outline-lime-300"
       >
         <RiLinkedinLine />
       </Link>
       <Link
-        href=""
+        href="https://github.com/wave-nature"
+        target="_blank"
         className=" hover:text-accent transition-all outline-lime-300"
       >
         <RiGithubLine />
