b1a039866238277699e8528ae3c51cedd5aa0c7c
wave-nature <vs1010582@gmail.com>
2023-11-01 18:09:39 +0530

    title change

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,15 @@ import { AnimatePresence, motion } from "framer-motion";
 import Layout from "../components/Layout";
 import Transition from "../components/Transition";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
     <Layout>
+      <Head>
+        <title>Vivek S. Bhadouria</title>
+      </Head>
       <AnimatePresence mode="wait">
         <motion.div key={router.route} className="h-full">
           <Transition />
